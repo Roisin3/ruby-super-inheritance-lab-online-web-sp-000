83e79069e94b231050eb24e3ed564ee78ed27b33
Michael Lynch <michael@michaelrlynch.com>
2019-09-27 02:52:24 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,2 +1,5 @@
 class ChattyStudent < Student
+  def hello
+    super
+    puts "how are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last ngiht? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
